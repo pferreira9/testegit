7b25d275b0570f7aa78e7e9ffc694773d4417751
pferreira9 <79099970+pferreira9@users.noreply.github.com>
2021-02-15 16:16:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Olá mundo estou a funcionar',
-  });
+  res.status(200).send({message: 'Olá mundo estou a funcionar',});
+});
+
+app.get('/ola', (req, res) => {
+  res.status(200).send({message: 'Oii, eu sou a rota Olá',});
 });
 
 app.listen(3001, () => {
